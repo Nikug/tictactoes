26cca7dcfe330cabe09edac029f65834950e6424
Niku Grönberg <gronbergniku@gmail.com>
2023-04-14 19:45:12 +0300

    init template, add prettier and eslint

---
+++ uno.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig, presetUno } from "unocss";
+import presetWebFonts from "@unocss/preset-web-fonts";
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: "google",
+      fonts: {
+        sans: "Open Sans",
+      },
+    }),
+  ],
+});
