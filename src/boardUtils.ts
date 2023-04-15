941950a198268299fcc38ea55bf6cb59065fcd4f
Niku Grönberg <gronbergniku@gmail.com>
2023-04-15 14:11:40 +0300

    add diagonal checking and tests

--- src/boardUtils.ts
+++ src/boardUtils.ts
@@ -1,4 +1,4 @@
-import { Board, Player, Vector2 } from './types'
+import { Board, Box, Player, Vector2 } from './types'
 
 interface CheckLine {
   playerId: Player['id'] | null
@@ -6,10 +6,19 @@ interface CheckLine {
 }
 
 export const checkBoard = (board: Board, winLength: number) => {
+  if (winLength <= 0) return []
+
   const wins = checkLines(board, winLength, 'horizontal')
+
   if (winLength > 1) {
     const verticalWins = checkLines(board, winLength, 'vertical')
     if (verticalWins.length) wins.push(...verticalWins)
+
+    const diagonalWins = checkDiagonals(board, winLength, 'topRightToBottomLeft')
+    if (diagonalWins.length) wins.push(...diagonalWins)
+
+    const otherDiagonalWins = checkDiagonals(board, winLength, 'bottomRightToTopLeft')
+    if (otherDiagonalWins.length) wins.push(...otherDiagonalWins)
   }
 
   return wins
@@ -32,21 +41,7 @@ export const checkLines = (
       const y = direction === 'horizontal' ? outer : inner
       const box = board.board[y][x]
 
-      if (!box.playerId) {
-        if (checkLine.positions.length >= winLength) {
-          wins.push({ ...checkLine })
-        }
-        checkLine = emptyCheckLine()
-        continue
-      }
-
-      if (box.playerId === checkLine.playerId || checkLine.playerId === null) {
-        checkLine.playerId = box.playerId
-        checkLine.positions.push({ x, y })
-      } else {
-        checkLine.playerId = box.playerId
-        checkLine.positions = [{ x, y }]
-      }
+      checkLine = checkBox(checkLine, wins, box, winLength)
     }
 
     if (checkLine.positions.length >= winLength) {
@@ -56,5 +51,84 @@ export const checkLines = (
 
   return wins
 }
+
+export const checkDiagonals = (
+  board: Board,
+  winLength: number,
+  direction: 'topRightToBottomLeft' | 'bottomRightToTopLeft'
+): CheckLine[] => {
+  const wins: CheckLine[] = []
+  const trtbl = direction === 'topRightToBottomLeft'
+
+  for (let x = 0; x < board.dimensions.x; x++) {
+    let checkLine: CheckLine = emptyCheckLine()
+    let innerY = trtbl ? 0 : board.dimensions.y - 1
+    let innerX = x
+    while (isInBounds({ x: innerX, y: innerY }, board.dimensions)) {
+      const box = board.board[innerY][innerX]
+      innerX++
+      innerY += trtbl ? 1 : -1
+
+      checkLine = checkBox(checkLine, wins, box, winLength)
+    }
+
+    if (checkLine.positions.length >= winLength) {
+      wins.push({ ...checkLine })
+    }
+  }
+
+  // Don't re-check the longest diagonal, so start at 1 or length-2.
+  let y = trtbl ? 1 : board.dimensions.y - 2
+  for (; y < board.dimensions.y && y >= 0; y += trtbl ? 1 : -1) {
+    let checkLine: CheckLine = emptyCheckLine()
+    let innerY = y
+    let innerX = 0
+    while (isInBounds({ x: innerX, y: innerY }, board.dimensions)) {
+      const box = board.board[innerY][innerX]
+      innerX++
+      innerY += trtbl ? 1 : -1
+
+      checkLine = checkBox(checkLine, wins, box, winLength)
+    }
+
+    if (checkLine.positions.length >= winLength) {
+      wins.push({ ...checkLine })
+    }
+  }
+
+  return wins
+}
+
+/**
+ * Checks current box and updates check line and wins accordingly.
+ * Note: mutates check line and win arrays.
+ */
+const checkBox = (
+  checkLine: CheckLine,
+  wins: CheckLine[],
+  box: Box,
+  winLength: number
+): CheckLine => {
+  if (!box.playerId) {
+    if (checkLine.positions.length >= winLength) {
+      wins.push({ ...checkLine })
+    }
+    checkLine = emptyCheckLine()
+    return checkLine
+  }
+
+  if (box.playerId === checkLine.playerId || checkLine.playerId === null) {
+    checkLine.playerId = box.playerId
+    checkLine.positions.push({ x: box.position.x, y: box.position.y })
+  } else {
+    checkLine.playerId = box.playerId
+    checkLine.positions = [{ x: box.position.x, y: box.position.y }]
+  }
+
+  return checkLine
+}
+
+const isInBounds = (point: Vector2, dimensions: Vector2) =>
+  point.x >= 0 && point.x < dimensions.x && point.y >= 0 && point.y < dimensions.x
 
 const emptyCheckLine = (): CheckLine => ({ playerId: null, positions: [] })
--- tests/boardUtils.spec.ts
+++ tests/boardUtils.spec.ts
@@ -1,6 +1,6 @@
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
-import { checkLines } from '../src/boardUtils'
+import { checkBoard, checkDiagonals, checkLines } from '../src/boardUtils'
 import { createGame } from '../src/GameLogic'
 
 test('Horizontal checking finds winning row', () => {
@@ -106,6 +106,89 @@ test("Vertical checking doesn't find winning row", () => {
   const wins = checkLines(game.board, game.winLength, 'vertical')
 
   assert.is(wins.length, 0)
+})
+
+test('Diagonal (\\) checking finds winning row', () => {
+  const playerId = 'p1'
+  const game = createGame({ x: 3, y: 3 }, 3)
+  game.board.board[0][0].playerId = playerId
+  game.board.board[1][1].playerId = playerId
+  game.board.board[2][2].playerId = playerId
+
+  const wins = checkDiagonals(game.board, game.winLength, 'topRightToBottomLeft')
+
+  assert.is(wins.length, 1)
+  assert.is(wins[0].playerId, playerId)
+  assert.is(wins[0].positions.length, 3)
+  assert.ok(wins[0].positions.some((position) => position.x === 0 && position.y === 0))
+  assert.ok(wins[0].positions.some((position) => position.x === 1 && position.y === 1))
+  assert.ok(wins[0].positions.some((position) => position.x === 2 && position.y === 2))
+})
+
+test('Diagonal (\\) checking finds multiple winning rows for multiple players', () => {
+  const playerId = 'p1'
+  const player2Id = 'p2'
+  const game = createGame({ x: 6, y: 6 }, 3)
+  game.board.board[0][0].playerId = playerId
+  game.board.board[1][1].playerId = playerId
+  game.board.board[2][2].playerId = playerId
+  game.board.board[3][3].playerId = playerId
+  game.board.board[3][0].playerId = player2Id
+  game.board.board[4][1].playerId = player2Id
+  game.board.board[5][2].playerId = player2Id
+
+  const wins = checkDiagonals(game.board, game.winLength, 'topRightToBottomLeft')
+
+  assert.is(wins.length, 2)
+  const player1Win = wins.find((win) => win.playerId === playerId)
+  const player2Win = wins.find((win) => win.playerId === player2Id)
+  assert.ok(player1Win)
+  assert.ok(player2Win)
+
+  assert.ok(player1Win.positions.some((position) => position.x === 0 && position.y === 0))
+  assert.ok(player1Win.positions.some((position) => position.x === 1 && position.y === 1))
+  assert.ok(player1Win.positions.some((position) => position.x === 2 && position.y === 2))
+  assert.ok(player1Win.positions.some((position) => position.x === 3 && position.y === 3))
+
+  assert.ok(player2Win.positions.some((position) => position.x === 0 && position.y === 3))
+  assert.ok(player2Win.positions.some((position) => position.x === 1 && position.y === 4))
+  assert.ok(player2Win.positions.some((position) => position.x === 2 && position.y === 5))
+})
+
+test('Diagonal (/) checking finds winning row', () => {
+  const playerId = 'p1'
+  const game = createGame({ x: 3, y: 3 }, 3)
+  game.board.board[2][0].playerId = playerId
+  game.board.board[1][1].playerId = playerId
+  game.board.board[0][2].playerId = playerId
+
+  const wins = checkDiagonals(game.board, game.winLength, 'bottomRightToTopLeft')
+
+  assert.is(wins.length, 1)
+  assert.is(wins[0].playerId, playerId)
+  assert.is(wins[0].positions.length, 3)
+  assert.ok(wins[0].positions.some((position) => position.x === 0 && position.y === 2))
+  assert.ok(wins[0].positions.some((position) => position.x === 1 && position.y === 1))
+  assert.ok(wins[0].positions.some((position) => position.x === 2 && position.y === 0))
+})
+
+test('CheckBoard finds all wins on full board', () => {
+  const playerId = 'p1'
+  const game = createGame({ x: 3, y: 3 }, 3)
+  game.board.board[0][0].playerId = playerId
+  game.board.board[0][1].playerId = playerId
+  game.board.board[0][2].playerId = playerId
+  game.board.board[1][0].playerId = playerId
+  game.board.board[1][1].playerId = playerId
+  game.board.board[1][2].playerId = playerId
+  game.board.board[2][0].playerId = playerId
+  game.board.board[2][1].playerId = playerId
+  game.board.board[2][2].playerId = playerId
+
+  const wins = checkBoard(game.board, game.winLength)
+
+  assert.is(wins.length, 8)
+  assert.ok(wins.every((win) => win.playerId === playerId))
 })
 
 test.run()
